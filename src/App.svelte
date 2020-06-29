820780407918f7cbb78991e28c61c7801851de83
Piotr Pawlica <piotr.pawlica@codeconcept.pl>
2020-06-29 15:35:18 +0200

    got populate game board with data

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,20 @@
     import Space from './componnets/Space.svelte'
     import gameStore from './stores/game-store.js'
 
+    let board = ["", "", "", "", "", "", "", "", ""];
+    let nextPlayer = "";
+    let winner = "";
+    let numberOfPeople = 0;
+
     gameStore.subscribe(data => {
-        console.log(data, 'data from store');
-    })
+        console.log(data);
+        if (!data) { return; }
+
+       winner = data.winner;
+       nextPlayer = data.nextPlayer;
+       board = data.board;
+       numberOfPeople = data.numberOfPeeps;
+    });
 </script>
 
 <style>
@@ -38,20 +49,27 @@
 
 <main>
     <h1>Tic Tak Toe</h1>
-    <h2>Number of people playing:</h2>
-    <h2>Player X</h2>
-
+    <h2>Number of people playing: {numberOfPeople}</h2>
+    {#if winner ==='TIE'}
+    <h2>Tie Game!!!</h2>
+    {:else if winner}
+    <h2>Player {winner} won!!!</h2>
+    {:else}
+    <h2>Player {nextPlayer}</h2>
+    {/if}
     <div class="game-board">
-        <Space space="X" />
-        <Space space="X" />
-        <Space space="X" />
-        <Space space="X" />
-        <Space space="X" />
-        <Space space="X" />
-        <Space space="X" />
-        <Space space="X" />
-        <Space space="X" />
+        <Space space={board[0]} />
+        <Space space={board[1]} />
+        <Space space={board[2]} />
+        <Space space={board[3]} />
+        <Space space={board[4]} />
+        <Space space={board[5]} />
+        <Space space={board[6]} />
+        <Space space={board[7]} />
+        <Space space={board[8]} />
     </div>
+    {#if winner}
     <button>New Game</button>
+    {/if}
 </main>
 

27a25dc83be06adeea6df8083d14a92875b79b0f
Piotr Pawlica <piotr.pawlica@codeconcept.pl>
2020-06-29 12:51:05 +0200

    got board game template created

---
+++ src/App.svelte
@@ -0,0 +1,52 @@
+<script>
+    import Space from './componnets/Space.svelte'
+</script>
+
+<style>
+    main {
+        width: 475px;
+        margin: 0 auto;
+    }
+
+    .game-board {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: repeat(3, 1fr);
+    }
+
+    button {
+        width: 100%;
+        margin-top: 20px;
+        padding: 10px 0;
+        border: none;
+        border-radius: 0;
+        background-color: lightblue;
+        font-size: 30px;
+        cursor: pointer;
+    }
+    button:hover {
+        outline: none;
+    }
+
+
+</style>
+
+<main>
+    <h1>Tic Tak Toe</h1>
+    <h2>Number of people playing:</h2>
+    <h2>Player X</h2>
+
+    <div class="game-board">
+        <Space space="X" />
+        <Space space="X" />
+        <Space space="X" />
+        <Space space="X" />
+        <Space space="X" />
+        <Space space="X" />
+        <Space space="X" />
+        <Space space="X" />
+        <Space space="X" />
+    </div>
+    <button>New Game</button>
+</main>
+
---
+++ src/componnets/Space.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let space = "";
+</script>
+
+<style>
+    .space {
+        border: 1px solid #666565;
+        height: 125px;
+        width: auto;
+        font-size: 75px;
+        text-align: center;
+        line-height: 125px;
+        cursor: pointer;
+    }
+</style>
+
+<div class="space">
+    {space}
+</div>

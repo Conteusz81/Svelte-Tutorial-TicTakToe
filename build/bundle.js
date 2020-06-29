fb52739b1a3819fd93c0f75afd2db6d4c8832043
Piotr Pawlica <piotr.pawlica@codeconcept.pl>
2020-06-29 16:35:38 +0200

    Updates

--- build/bundle.js
+++ build/bundle.js
@@ -416,7 +416,7 @@ var app = (function () {
     const connect = () => {
         try {
             if (!connection || connection.readyState >= 2) {
-                connection = new WebSocket({"env":{"apiUrl":"https://github.tictactoe.com","wsUrl":"wss://github.tictactoe.com/ws"}}.env.wsUrl);
+                connection = new WebSocket({"env":{"apiUrl":"https://conteusz81.github.io/Svetle-Tutorial-TicTakToe/","wsUrl":"wss://conteusz81.github.io/Svetle-Tutorial-TicTakToe//ws"}}.env.wsUrl);
 
                 connection.onclose = e => {
                     console.log(e, 'closed');
@@ -464,7 +464,7 @@ var app = (function () {
 
     const nextMove = async space => {
         try {
-            const response = await fetch(`${{"env":{"apiUrl":"https://github.tictactoe.com","wsUrl":"wss://github.tictactoe.com/ws"}}.env.apiUrl}/next-turn/${space}`);
+            const response = await fetch(`${{"env":{"apiUrl":"https://conteusz81.github.io/Svetle-Tutorial-TicTakToe/","wsUrl":"wss://conteusz81.github.io/Svetle-Tutorial-TicTakToe//ws"}}.env.apiUrl}/next-turn/${space}`);
             const message = await response.json();
 
             return message.errorMessage;
@@ -476,7 +476,7 @@ var app = (function () {
 
     const reset = async () => {
         try {
-            await fetch(`${{"env":{"apiUrl":"https://github.tictactoe.com","wsUrl":"wss://github.tictactoe.com/ws"}}.env.apiUrl}/reset`);
+            await fetch(`${{"env":{"apiUrl":"https://conteusz81.github.io/Svetle-Tutorial-TicTakToe/","wsUrl":"wss://conteusz81.github.io/Svetle-Tutorial-TicTakToe//ws"}}.env.apiUrl}/reset`);
         } catch (e) {
             console.log(e);
             return 'Error connection to the server';
